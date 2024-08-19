b1ed9e9ee45126de92cb7cc33212d5b5132babbc
John Edward Oblepias <johnedwardoblepias11@gmail.com>
2024-08-19 19:12:41 +0800

    Initial projects section

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu () {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+const popoverContainer = document.querySelector('.popover-container');
+const popoverContent = document.querySelector('.popover-content');
+
+popoverContainer.addEventListener('mouseenter', () => {
+    popoverContent.style.display = 'block';
+});
+
+popoverContainer.addEventListener('mouseleave', () => {
+    popoverContent.style.display = 'none';
+});
