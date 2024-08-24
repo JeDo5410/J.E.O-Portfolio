be89799ad5b3f97c068ce9397cfd193c09fbe285
John Edward Oblepias <johnedwardoblepias11@gmail.com>
2024-08-24 15:57:50 +0800

    .

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function closeModal() {
 
 // Close the modal when clicking outside the image
 window.onclick = function(event) {
-    var modal = document.getElementById("imageModal");
+    var modal = document.getElementById("imageModal"); 
     if (event.target == modal) {
         modal.style.display = "none";
     }
